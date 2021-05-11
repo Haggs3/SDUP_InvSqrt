bce72c22d5edd4d654b1ce397142d1ebd33e6de7
LB197 <predat97@gmail.com>
2021-05-11 12:04:08 +0200

    merged IDLE and MUL states

--- Vivado/source/float_mul.v
+++ Vivado/source/float_mul.v
@@ -1,5 +1,4 @@
 `timescale 1ns / 1ps
-`define EXP_SHIFT 23
 
 module float_mul (clk, rst, start, float_in_1, float_in_2, float_out, ready);
 
@@ -13,17 +12,16 @@ output reg [31:0] float_out;
 output reg ready;
 
 wire signed [7:0] E1, E2;
-reg signed [7:0] E, exp;
 wire [22:0] M1, M2, M_trunc;
 reg [22:0] M;
 reg [47:0] M_mul;
-reg  [2:0]state;
+reg [1:0] state;
+reg signed [7:0] E;
 
-localparam IDLE = 3'b000,
-           MUL = 3'b001,
-           OVERFLOW = 3'b010,
-           ROUNDING = 3'b011,
-           FINISH = 3'b100;
+localparam IDLE_MUL = 3'b00,
+           OVERFLOW = 3'b01,
+           ROUNDING = 3'b10,
+           FINISH = 3'b11;
 
 assign E1 = float_in_1[30:23] - 127;
 assign E2 = float_in_2[30:23] - 127;
@@ -33,40 +31,35 @@ assign M2 = float_in_2[22:0];
 
 assign M_trunc = M_mul[22:0];
 
-always @ (posedge clk)
-                                                 
+always @ (posedge clk)                                               
     if(rst == 1'b1) begin
         float_out <= 32'b0;
         ready <= 1'b0;
-        state <= IDLE;
-        exp <= 7'b0;
+        state <= IDLE_MUL;
         E <= 7'b0;
         M <= 23'b0;
         M_mul <= 48'b0;
     end else begin
         case(state)
-            IDLE: begin
+            IDLE_MUL: begin
                 E <= E1 + E2;
+                ready <= 1'b0;
                 if (start == 1'b1) begin
-                    ready <= 1'b0;
-                    state <= MUL;
+                    M_mul <= ({1'b1, M1} * {1'b1, M2});
+                    state <= OVERFLOW;
                 end else begin
-                    state <= IDLE;
+                    state <= IDLE_MUL;
                 end
             end
-            MUL:begin
-                M_mul <= ({1'b1, M1} * {1'b1, M2});
-                state <= OVERFLOW;
-            end
             OVERFLOW: begin
-            if (M_mul[47] == 1) begin
+                if (M_mul[47] == 1) begin
                     M_mul <= M_mul >> 1;
                     E <= E + 1;
                 end else begin
                     M_mul <= M_mul;
                     E <= E;
                 end
-            state <= ROUNDING;
+                state <= ROUNDING;
             end
             ROUNDING: begin
                 if(M_trunc[22] == 1) begin
@@ -74,12 +67,13 @@ always @ (posedge clk)
                 end else begin
                     M <= M_mul[45:23];
                 end
-            state <= FINISH;
+                state <= FINISH;
             end
             FINISH: begin
-                float_out <= {(E + 127), M};
+                float_out[30:0] <= {(E + 127), M};
+                float_out[31] <= 1'b0;
                 ready <= 1'b1;
-                state <= IDLE;
+                state <= IDLE_MUL;
             end
         endcase
     end
